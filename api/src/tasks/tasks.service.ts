5e94d3e2606b9a7d0403de96fe376a1bfe7fe7c6
Luiyi Valentín <luiyivalentin18@gmail.com>
2020-07-29 22:20:29 -0400

    Added frontend

---
+++ api/src/tasks/tasks.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TaskStatus } from './task-status.enum';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
+import { Task } from './task.entity';
+import { User } from 'src/auth/user.entity';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(TaskRepository) private taskRepository: TaskRepository,
+  ) {}
+
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+    return this.taskRepository.getTask(filterDto, user);
+  }
+
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const foundTask = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
+
+    if (!foundTask) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    return foundTask;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto, user);
+  }
+
+  async deleteTask(id: number, user: User): Promise<void> {
+    const result = await this.taskRepository.delete({ id, userId: user.id });
+
+    if (!result.affected) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+  }
+
+  async updateTaskStatus(
+    id: number,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
+    task.status = status;
+    await task.save();
+    return task;
+  }
+}
